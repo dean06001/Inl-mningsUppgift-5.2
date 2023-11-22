a2e6e540de41932eb28b72135c6e0c1ef992458b
dean06001 <dean06001@E-PW0234KC>
2023-11-22 10:01:13 +0100

    Add project files.

---
+++ InlämningsUppgift 5.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace InlämningsUppgift_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tal = new string[3];
+            tal[0] = "5";
+            tal[1] = "13";
+            Console.WriteLine($"Skriv in ett tal förutom {tal[0]} och {tal[1]}");
+            tal[2] = Console.ReadLine();
+
+            Console.WriteLine("Här är de tre talen");
+            Console.WriteLine($"{tal[0]}, {tal[1]} och {tal[2]}");
+            Console.ReadKey();
+        }
+    }
+}
